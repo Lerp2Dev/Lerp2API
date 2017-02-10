2955eae046df9ce4290514486c832aca5aa8e94f
Ikillnukes <ikillnukes1@gmail.com>
2017-02-10 08:10:01 +0100

    Changes

--- Compile/Console/compile_console.bat
+++ Compile/Console/compile_console.bat
@@ -2,7 +2,8 @@
 
 if "%MAIN_PATH%" == "" ( for %%a in ("%~dp0../..") do set "MAIN_PATH=%%~fa" )
 if "%REF_PATH%" == "" ( set "REF_PATH=%MAIN_PATH%\Assemblies" )
+set "msbuildPath=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\amd64"
 
-"%~dp0\MSBuild.exe" "%MAIN_PATH%\Lerp2API.sln" /t:Lerp2Console /pp "/p:ReferencePath=%REF_PATH%" /p:Platform="Any CPU" /p:OutputPath=../../Build/Console /p:Configuration=Debug /flp:logfile=Output.log;verbosity=diagnostic  
+"%msbuildPath%\MSBuild.exe" "%MAIN_PATH%\Lerp2API.sln" /t:Lerp2Console /pp "/p:ReferencePath=%REF_PATH%" /p:Platform="Any CPU" /p:OutputPath=../../Build/Console /p:Configuration=Debug /flp:logfile=Output.log;verbosity=diagnostic  
 
 pause
