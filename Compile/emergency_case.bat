aa079c1900a373982b3d179686e7f198ca9291f9
Ikillnukes <ikillnukes1@gmail.com>
2017-02-13 22:43:03 +0100

    Improved emergency_case.bat thanks to ElektroStudios, now the problem is in compile.bat

--- Compile/emergency_case.bat
+++ Compile/emergency_case.bat
@@ -1,40 +1,46 @@
-@echo off
-::Call this in case your broke you dll and you cannot start Unity for example
+@Echo OFF
 
+::Solution: http://stackoverflow.com/questions/42211530/batch-error-was-not-expected-at-this-time/42213519#42213519
+::Call this in case you've broke you version of the API and you cannot start Unity...
+ 
 :str
-cls
-goto :main
-
-:main
-echo Hello, well, if you're opening this is because you have done something wrong and you cannot start Unity...
-echo.
-set /p opt="Do you want to read your path from the 'path.txt' file or do you want to specify? [Y/N] "
-echo.
-echo Also, this is optional but you can try to establish an order for the compilation.
-echo.
-echo 1.- Build the API
-echo 2.- Build the RAW Scripts
-echo 3.- Build the Editor API
-echo.
-set /p order="Type, for example: [2 1 3], to compile in this order, or the way you want: "
-
-if /i "%opt%" == "Y" (
-	for /f "delims=" %%f in ("project_path.txt") do (
-		if "%%f" NEQ "" (
-			call :callcompile "%%f" "%order%"
-		)
-	)
-) else (
-	if /i "%opt%" == "N" (
-		echo.
-		set /p cpath="Path: "
-		goto :callcompile "%cpath%" "%order%"
-	) else (
-		goto :str
-	)
+CLS
+GoTo :Main
+ 
+:Main
+Echo:Hello. Well, if you're opening this it is because you have done something wrong and you cannot start Unity...
+Echo+
+Choice.exe /C "YN" /M "Do you want to read your path from the \"project_path.txt\" file or do you want to specify "
+Set /A "opt=%ErrorLevel%" :: 1=Y, 2=N
+Echo+
+Echo:Also, these options below are optional but you can try to establish an order for the compilation.
+Echo+
+Echo:1.- Build the API
+Echo:2.- Build the RAW Scripts
+Echo:3.- Build the Editor API
+Echo+
+Set /P "order=Type, for example: [2 1 3], to compile in this order, or the way you want: "
+ 
+If %opt% EQU 1 (
+    FOR /F "Delims=" %%f in (
+    	"project_path.txt"
+    ) Do (
+        If "%%~f" NEQ "" (
+            Call :RunCompiler "%%~f" "%order%"
+        )
+    )
+ 
+) Else (
+    Set /P "cpath=Path: "
+    Call :RunCompiler "%cpath%" "%order%"
+ 
 )
-goto :EOF
-
-:callcompile
-cmd /c compile.bat "%~1" "%~2"
-pause
+GoTo :Terminate
+ 
+:RunCompiler
+echo aaaa
+CMD.exe /C " compile.bat "%~1" "%~2" "
+GoTo :EOF
+ 
+:Terminate
+Pause&Exit
